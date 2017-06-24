6a40f0ebea92fd93ab52becb18df9f984d15773d
ibarram2 <scorpiobox@hotmail.com>
2017-06-24 04:10:06 -0700

    incomplete

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'lwEMNKmodI9RD47cwsny2u9wH',
+  consumer_secret: 'guz3DeXnTHGGutOR1dm5tOo3rtFX0aHDhK514DhlASeBdnuAjV',
+  access_token_key: '	878529698443087872-P0JQYQRRrHFADibaPxTqidzS6sXD7ml',
+  access_token_secret: 'ZX6cTd6JVGIMbKWZPuo19SQAmkNrr26a0TSdk1s0PqGq4',
+}
---
+++ liri.js
@@ -0,0 +1,21 @@
+'use strict';
+(function(){
+
+	var keys = require("./keys");
+	console.log(keys);
+
+})();
+
+//I know I need to make npm requests now to retrieve the information that is required of the commands outlined in the HW.
+//it's 4 am and I'm sorry for not having more time to follow through.
+//the code I need to modfy to access these API's is below.
+
+//var request = require('request');
+//request('http://www.google.com', function (error, response, body) {
+  //console.log('error:', error); // Print the error if one occurred 
+  //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
+  //console.log('body:', body); // Print the HTML for the Google homepage. 
+//});
+
+//i would then need to write code capable of taking arguments from the user to pull that information from the API and then console log it back to the user.
+//i know there isn't much. sorry.
